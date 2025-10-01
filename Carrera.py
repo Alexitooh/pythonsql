9fc5d1fbfb3fea2c21d9b06551c78a804c8e5bb9
Marc Montoya <marc200113@gmail.com>
2025-10-01 19:41:24 +0200

    class Carrera

---
+++ Carrera.py
@@ -0,0 +1,34 @@
+class Carrera:
+    def __init__(self, id, nombre, duracion, nota_corte, area_conocimiento, modalidad): 
+        self.id = id
+        self.nombre = nombre
+        self.duracion = duracion
+        self.nota_corte = nota_corte
+        self.area_conocimiento = area_conocimiento
+        self.modalidad = modalidad
+    
+    def getId(self): 
+        return self.id
+    def getNombre(self):
+        return self.nombre
+    def getDuracion(self):
+        return self.duracion
+    def getNotaCorte(self):
+        return self.nota_corte
+    def getAreaConocimineto(self):
+        return self.area_conocimiento
+    def getModalidad(self): 
+        return self.modalidad
+    
+    def setId(self, id):
+        self.id = id
+    def setNombre(self,nombre):
+        self.nombre = nombre
+    def setDuracion(self,duracion):
+        self.duracion = duracion
+    def setNotaCorte(self, nota_corte):
+        self.nota_corte = nota_corte
+    def setAreaConocimiento(self, area_conocimiento):
+        self.area_conocimiento = area_conocimiento
+    def setModalidad(self, modalidad): 
+        self.modalidad = modalidad
