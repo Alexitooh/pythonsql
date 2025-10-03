c744b42f78d9532d19556f6baca5b4e4ee8abdec
Alexis <carlos.alexiss_6@hotmail.com>
2025-10-03 16:42:25 +0200

    Archivo agregado

--- DAO.py
+++ DAO.py
@@ -5,40 +5,37 @@ mydb = mysql.connector.connect(
     user="root",
     password="123456",
     database="carrera"
-)
-class carreradao:
+)   
+def selectAll(): 
+    mycursor = mydb.cursor()
+    sql = "SELECT * FROM carrera"
+    mycursor.execute(sql)
+    return mycursor.fetchall()
 
-    def selectAll(): 
-        mycursor = mydb.cursor()
-        sql = "SELECT * FROM carrera"
-        mycursor.execute(sql)
-        result = mycursor.fetchall()
-        for x in result:
-            print(x)
+def insert(carrera:Carrera): 
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO carrera(nombre,duracion, nota_corte, area_conocimineto, modalidad ) VALUES(%s, %s,%s, %s, %s)"
+    mycursor.execute(sql, (carrera.getNombre(), carrera.getDuracion(), carrera.getNotaCorte(), carrera.getAreaConocimineto(), carrera.getModalidad()))
+    mydb.commit()
+    mycursor2 = mydb.cursor("SELECT id from carrera where nombre = %s", (carrera.getNombre(),))
+    return mycursor2.fetchone()
 
-    def insert(name): 
-        mycursor = mydb.cursor()
-        sql = "INSERT INTO carrera (nombre) VALUES(%s)"
-        mycursor.execute(sql, (name))
-        mydb.commit()
-        print(mycursor.rowcount, "record inserted.")
+def update(name, campo, newName):
+    mycursor = mydb.cursor()
+    sql = "UPDATE carrera set %s = %s WHERE nombre = %s"
+    mycursor.execute(sql, (campo, newName, name))
+    mydb.commit()
+    print(mycursor.rowcount, "record(s) affected")
 
-    def update(name, newName):
-        mycursor = mydb.cursor()
-        sql = "UPDATE carrera set nombre = %s WHERE nombre = %s"
-        mycursor.execute(sql, (newName, name))
-        mydb.commit()
-        print(mycursor.rowcount, "record(s) affected")
+def delete(name):
+    mycursor = mydb.cursor()
+    sql = "DELETE FROM carrera WHERE nombre = %s"
+    mycursor.execute(sql, (name,))
+    mydb.commit()
+    print(mycursor.rowcount, "record(s) deleted")
 
-    def delete(name):
-        mycursor = mydb.cursor()
-        sql = "DELETE FROM carrera WHERE nombre = %s"
-        mycursor.execute(sql, (name,))
-        mydb.commit()
-        print(mycursor.rowcount, "record(s) deleted")
-
-car = carreradao ()
-
-car.insert ()
-car.update()
-car.delete()
+def wathcOne (nombre):
+    mycursor = mydb.cursor()
+    sql = "SELECT * FROM carrera"
+    mycursor.execute(sql)
+    return mycursor.fetchall()
